dc147bb748471a19992f9c8f13a496263aafd103
Ansar Khan <ansarkhan98015@gmail.com>
2025-08-28 21:44:16 +0530

    initial changes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  mobileNumber: string = '+91 9776718582';
   constructor() { }
 
   ngOnInit() {
