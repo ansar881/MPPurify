3b3ad64e197252fe5e7a19444245c488d75aff58
Ansar Khan <ansarkhan98015@gmail.com>
2025-11-30 11:10:10 +0530

    About page developed

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,8 +11,9 @@ import { Observable } from 'rxjs';
 export class AboutComponent {
 
   tophTitle = 'Delivering Purity, One Drop at a Time — A Legacy of Trust, Innovation, and Care That Hydrates Millions Every Day';
-  juorneyTitle = 'The MP Purify Juorney';
-  journeyContent = 'At MP Purify, our journey began with a simple yet profound mission: to provide pure, safe, and refreshing drinking water to communities everywhere. Founded on the principles of trust, innovation, and care, we have dedicated ourselves to ensuring that every drop of water we deliver meets the highest standards of quality and purity. Over the years, we have expanded our reach, embracing cutting-edge technology and sustainable practices to minimize our environmental impact. Our commitment to excellence is unwavering, as we continuously strive to enhance our products and services. Today, MP Purify stands as a beacon of reliability and health, hydrating millions of lives daily while fostering a legacy of trust and well-being for generations to come.';
+  juorneyTitle = 'The Jaaal Life Juorney';
+  journeyContent = 'At Jaaal Life, our journey began with a simple yet profound mission: to provide pure, safe, and refreshing drinking water to communities everywhere. Founded on the principles of trust, innovation, and care, we have dedicated ourselves to ensuring that every drop of water we deliver meets the highest standards of quality and purity. Over the years, we have expanded our reach, embracing cutting-edge technology and sustainable practices to minimize our environmental impact. Our commitment to excellence is unwavering, as we continuously strive to enhance our products and services. Today, Jaaal Life stands as a beacon of reliability and health, hydrating millions of lives daily while fostering a legacy of trust and well-being for generations to come.';
 
+  ceoQuote = "“At Jaaal Life, we don’t just bottle water — we bottle trust, purity, and a healthier tomorrow.”"
   constructor() {}
 }
--- src/app/components/faqs/faqs.component.ts
+++ src/app/components/faqs/faqs.component.ts
@@ -17,7 +17,7 @@ export class FaqsComponent implements OnInit {
 
   faqs = [
     {
-      title: "How is freeAqua4All Free?",
+      title: "How is Jaaal Life Free?",
       description: "We provide drinking water, with the cost subsidized through advertisements covering 80% of the bottle's surface. Consumers pay only for the water, while advertisers cover the remaining expenses."
     },
     {
@@ -25,7 +25,7 @@ export class FaqsComponent implements OnInit {
       description: "We are continuously working to offer nationwide water bottle advertising services. Currently, our services are available across Hyderabad."
     },
     {
-      title: "How can I help freeAqua4All?",
+      title: "How can I help Jaaal Life?",
       description: "Please help us to spread the word by sharing our project on social media and with your friends and family."
     },
     {
@@ -37,8 +37,8 @@ export class FaqsComponent implements OnInit {
       description: "The preparation time of an order along with the label design finalization, label printing, getting the bottles ready, packaging, and moving them to the distributors takes around 10-12 days. So, within 16 days of placing an order with free aqua for all your water ads will hit the market."
     },
     {
-      title: "How does freeAqua4All compare to other advertising mediums?",
-      description: "freeAqua4All offers a **unique, high-visibility** advertising solution compared to traditional mediums like billboards, digital ads, or print media. Here's how it stands out: *Direct Consumer Engagement:** Your brand reaches consumers in a **tangible way** as they hold and drink from branded water bottles. *Higher Retention:** Unlike fleeting digital ads, your branding stays visible for **several minutes** while the consumer drinks the water. *Cost-Effective:** Compared to billboards or TV ads, water bottle advertising provides **affordable, widespread exposure** with **less ad fatigue**. **Targeted Distribution:** You can place bottles at **strategic locations** (events, offices, gyms, public areas) to reach the **right audience**. *Eco-Friendly Image:** Associating with drinking water enhances your brand's **social responsibility** and goodwill."
+      title: "How does Jaaal Life compare to other advertising mediums?",
+      description: "Jaaal Life offers a **unique, high-visibility** advertising solution compared to traditional mediums like billboards, digital ads, or print media. Here's how it stands out: *Direct Consumer Engagement:** Your brand reaches consumers in a **tangible way** as they hold and drink from branded water bottles. *Higher Retention:** Unlike fleeting digital ads, your branding stays visible for **several minutes** while the consumer drinks the water. *Cost-Effective:** Compared to billboards or TV ads, water bottle advertising provides **affordable, widespread exposure** with **less ad fatigue**. **Targeted Distribution:** You can place bottles at **strategic locations** (events, offices, gyms, public areas) to reach the **right audience**. *Eco-Friendly Image:** Associating with drinking water enhances your brand's **social responsibility** and goodwill."
     },
     {
       title: "Does Free Aqua for All distribute customized water bottles?",
@@ -46,7 +46,7 @@ export class FaqsComponent implements OnInit {
     },
     {
       title: "How do I advertise?",
-      description: "To advertise with freeAqua4All, please visit https://freeAqua4All.com"
+      description: "To advertise with Jaaal Life, please visit https://Jaaal Life.com"
     }
   ];
 
